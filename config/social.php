ec1dd8317d0e9c97b629d41e0399092b3e0b3291
unknown <arnaldo.nhaguilunguane@isutc.ac.mz>
2020-11-30 14:50:45 +0200

    Alterar o pixel que a eagadgets deu

--- config/social.php
+++ config/social.php
@@ -3,7 +3,7 @@
 return [
     'whatsapp_number'=>'+258843897904',
     'phone_number' => '+258843897904',
-    'pixel' => '366993480935221',
+    'pixel' => '411279496665107',
     'app_domain' => 'eagadgets.com',
     ];
     
