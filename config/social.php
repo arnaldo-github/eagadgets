ef9c4da3054509298a191350215884ed5472d3a3
unknown <arnaldo.nhaguilunguane@isutc.ac.mz>
2020-11-30 13:44:10 +0200

    Teste com o meu pixel

--- config/social.php
+++ config/social.php
@@ -3,7 +3,7 @@
 return [
     'whatsapp_number'=>'+258843897904',
     'phone_number' => '+258843897904',
-    'pixel' => '366993480935221',
+    'pixel' => '3663972583662029',
     'app_domain' => 'eagadgets.com',
     ];
     
