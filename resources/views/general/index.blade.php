4bab96b14f0e7f27921f28c675de0d442f64d374
unknown <arnaldo.nhaguilunguane@isutc.ac.mz>
2020-11-30 14:03:59 +0200

    Adicionar Pixel a pagina inicial

--- resources/views/general/index.blade.php
+++ resources/views/general/index.blade.php
@@ -55,4 +55,24 @@
 
 	@Include('components-structure.marketing')
 </div>
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/pt_PT/sdk.js#xfbml=1&version=v9.0" nonce="BAxxxEfF"></script>
+
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+ fbq('init', {{Illuminate\Support\Facades\Config::get('social.pixel')}}); 
+fbq('track', 'PageView');
+</script>
+<noscript>
+ <img height="1" width="1" 
+src="https://www.facebook.com/tr?id={{Illuminate\Support\Facades\Config::get('social.pixel')}}&ev=PageView
+&noscript=1"/>
+</noscript>
 @endsection
