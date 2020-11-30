0298ba1c6a3ac78b1123e55fd08ed2d778e37903
unknown <arnaldo.nhaguilunguane@isutc.ac.mz>
2020-11-30 13:54:33 +0200

    Teste com o pixel da eagadgets

--- config/social.php
+++ config/social.php
@@ -3,7 +3,7 @@
 return [
     'whatsapp_number'=>'+258843897904',
     'phone_number' => '+258843897904',
-    'pixel' => '3663972583662029',
+    'pixel' => '366993480935221',
     'app_domain' => 'eagadgets.com',
     ];
     
