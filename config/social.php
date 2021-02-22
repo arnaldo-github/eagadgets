70fecc2cd28ee1860b36f409bf522813d8481649
Arnaldo Manuel <70331507+arnaldomanuel@users.noreply.github.com>
2021-02-22 12:12:18 +0200

    Mudança de pixel

--- config/social.php
+++ config/social.php
@@ -4,7 +4,7 @@ return [
     'whatsapp_number'=>'+258843897904',
     'phone_number' => '+258843897904',
     /*'pixel' => '411279496665107',*/
-    'pixel' => '208843520926534',
+    'pixel' => '411279496665107',
     'app_domain' => 'eagadgets.com',
     ];
     
